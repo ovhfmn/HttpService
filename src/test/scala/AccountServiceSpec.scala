1fc677007f4d629aff07d09e1aa8beb90b6365b2
alex <ovhfmn@gmail.com>
2026-04-22 00:26:10 +0200

    add tests AccountServiceSpec.scala

--- src/test/scala/AccountServiceSpec.scala
+++ src/test/scala/AccountServiceSpec.scala
@@ -1,47 +1,80 @@
 import cats.effect.IO
 import cats.effect.kernel.Ref
+import com.httpService.domain.Models.*
+import com.httpService.domain.Models.AccountId.AccountId
+import com.httpService.domain.Models.DomainError.InsufficientFunds
 import com.httpService.repository.InMemoryAccountRepository
-import com.httpService.domain.domain.*
-import com.httpService.domain.domain.AccountId.AccountId
-import com.httpService.domain.domain.DomainError.{AccountAlreadyExists, InsufficientFunds}
-import com.httpService.service.LiveAccountService
+import com.httpService.service.AccountService
 import munit.CatsEffectSuite
 
 class AccountServiceSpec extends CatsEffectSuite {
 
-  test("created account succeeds") {
+  def createService: IO[AccountService] =
     for {
       ref <- Ref.of[IO, Map[AccountId, Account]](Map.empty)
+    } yield new AccountService(new InMemoryAccountRepository(ref))
 
-      repo = new InMemoryAccountRepository(ref)
-      service = new LiveAccountService(repo)
 
-//      id = AccountId.from("account_1").toOption.get
-//      balance = Balance.from(BigDecimal(0)).toOption.get
-
+  test("create account succeeds") {
+    for {
+      service <- createService
       account <- service.create("account_1", BigDecimal(0)).value
     } yield {
       assertEquals(account.isRight, true)
     }
   }
 
-  test("credit increases balance") {}
+  test("create fails if duplicate") {
+    for {
+      service <- createService
+      account <- service.create("account_1", BigDecimal(0)).value
+    } yield {
+      assertEquals(account.isRight, true)
+    }
+  }
+
+  test("create fails on invalid id") {
+    for {
+      service <- createService
+      result <- service.create("", 100).value
+    } yield {
+      assert(result.isLeft)
+    }
+  }
+
+  test("create fails on negative balance") {
+    for {
+      service <- createService
+      result <- service.create("acc1", -100).value
+    } yield {
+      assert(result.isLeft)
+    }
+  }
+
+  test("credit increases balance") {
+    for {
+      service <- createService
+      _ <- service.create("acc1", 100).value
+      result <- service.credit("acc1", 50).value
+    } yield {
+      assertEquals(result.map(_.balance.value), Right(BigDecimal(150)))
+    }
+  }
+
+  test("credit fails if account does not exist") {
+    for {
+      service <- createService
+      result <- service.credit("unknown", 10).value
+    } yield {
+      assert(result.isLeft)
+    }
+  }
 
   test("debit reduces balance") {
     for {
-      ref <- Ref.of[IO, Map[AccountId, Account]](Map.empty)
-
-      repo = new InMemoryAccountRepository(ref)
-      service = new LiveAccountService(repo)
-
-      id = AccountId.from("account_2").toOption.get
-      balance = Balance.from(BigDecimal(100)).toOption.get
-      account = Account(id, balance)
-
-//      amount = Money.from(BigDecimal(10)).toOption.get
-
-      result = AccountService.debit(account, BigDecimal(10))
-
+      service <- createService
+      _ <- service.create("acc1", 100).value
+      result <- service.debit("acc1", 10).value
     } yield {
       assertEquals(result.map(_.balance.value), Right(BigDecimal(90)))
     }
@@ -49,39 +82,20 @@ class AccountServiceSpec extends CatsEffectSuite {
 
   test("debit fails if insufficient funds") {
     for {
-      ref <- Ref.of[IO, Map[AccountId, Account]](Map.empty)
-
-      repo = new InMemoryAccountRepository(ref)
-      service = new LiveAccountService(repo)
-
-      id = AccountId.from("account_3").toOption.get
-      balance = Balance.from(BigDecimal(50)).toOption.get
-      account = Account(id, balance)
-
-//      amount = Money.from(BigDecimal(100)).toOption.get
-
-      result = AccountService.debit(account, BigDecimal(100))
-
+      service <- createService
+      _ <- service.create("acc1", 50).value
+      result <- service.debit("acc1", 100).value
     } yield {
       assertEquals(result, Left(InsufficientFunds))
     }
   }
 
-  test("account w/ the same id should not be created twice") {
+  test("debit fails if account does not exist") {
     for {
-      ref <- Ref.of[IO, Map[AccountId, Account]](Map.empty)
-
-      repo = new InMemoryAccountRepository(ref)
-      service = new LiveAccountService(repo)
-
-      id = AccountId.from("account_1").toOption.get
-//      balance = Balance.from(BigDecimal(100)).toOption.get
-
-      first <- service.create("account_1", BigDecimal(100)).value
-      second <- service.create("account_1", BigDecimal(100)).value
+      service <- createService
+      result <- service.debit("unknown", 10).value
     } yield {
-      assertEquals(first.isRight, true)
-      assertEquals(second, Left(AccountAlreadyExists))
+      assert(result.isLeft)
     }
   }
 }
